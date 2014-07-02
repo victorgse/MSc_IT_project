8d159cdf22faf4980f3d0b403cb08557420773f0
victorgse <victorgse@gmail.com>
2014-07-02 23:22:29 +0100

    Updates to GUI

--- src/View.java
+++ src/View.java
@@ -1,4 +1,46 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class View {
-
+public class View extends JFrame {
+	
+	// instance variables
+	
+	// constructor
+	public View() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1140, 585);
+		//setLocation(100, 100);
+		//setResizable(false); // disables resizing of the JFrame window
+		setTitle("Machine Learning the Premier League");
+		
+		layoutTop();
+		layoutMiddle();
+		layoutBottom();
+	}
+	
+	private void layoutTop() {
+		JPanel top = new JPanel();
+		top.setBackground(Color.lightGray);
+		this.add(top, BorderLayout.NORTH);
+	}
+	
+	private void layoutMiddle() {
+		JLabel instructions_and_info = new JLabel();
+		instructions_and_info.setHorizontalAlignment(SwingConstants.CENTER);
+		//instructions_and_info.setVerticalAlignment(SwingConstants.TOP);
+		instructions_and_info.setFont(new Font("Serif", Font.BOLD + Font.ITALIC, 48));
+		instructions_and_info.setText("Which dataset do you wish to analyse?");
+		this.add(instructions_and_info, BorderLayout.CENTER);
+	}
+	
+	private void layoutBottom() {
+		JPanel bottom = new JPanel();
+		bottom.setPreferredSize(new Dimension(500, 100));
+		bottom.setBackground(Color.lightGray);
+		JComboBox datasets = new JComboBox();
+		datasets.setPreferredSize(new Dimension(300, 50));
+		datasets.addItem("MCFC Analytics Full Dataset");
+		bottom.add(datasets);
+		this.add(bottom, BorderLayout.SOUTH);
+	}
 }
