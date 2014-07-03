552df37cd6f6ee8671e0d8efb306f4f829c71e50
victorgse <victorgse@gmail.com>
2014-07-03 10:51:49 +0100

    Added Controller class

---
+++ src/Controller.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Controller implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
