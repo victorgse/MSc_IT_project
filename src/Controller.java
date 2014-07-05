2f40de764e1681bef3c2e059f21c8896e706684e
victorgse <victorgse@gmail.com>
2014-07-05 15:59:06 +0100

    Updates to GUI

--- src/Controller.java
+++ src/Controller.java
@@ -4,22 +4,75 @@ public class Controller implements ActionListener {
 	
 	// instance variables
 	private View viewObject;
-	// private Algorithm algorithmObject;
+	private String state;
+	
+	public Controller() {
+		state = "startScreen_1";
+	}
 	
 	public void setView(View view) {
 		viewObject = view;
 	}
 
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		if (ae.getSource() == viewObject.startOverButton) {
-			System.out.println("Start Over was clicked");
+			state = "startScreen_1";
+			viewObject.updateView(state);
 		} else if (ae.getSource() == viewObject.backButton) {
-			System.out.println("Back was clicked");
+			switch (state) {
+				case "startScreen_2":
+					state = "startScreen_1";
+					break;
+				case "clustering_step1":
+				case "classification_step1":
+				case "anomalyDetection_step1":
+					state = "startScreen_2";
+					break;
+				case "clustering_step2":
+					state = "clustering_step1";
+					break;
+				case "clustering_step3":
+					state = "clustering_step2";
+					break;
+				case "clustering_step4":
+					state = "clustering_step3";
+					break;
+			}
+			viewObject.updateView(state);
 		} else if (ae.getSource() == viewObject.nextButton) {
-			System.out.println("Next was clicked");
+			switch (state) {
+				case "startScreen_1":
+					state = "startScreen_2";
+					break;
+				case "startScreen_2":
+					if (viewObject.clusteringButton.isSelected()) {
+						state = "clustering_step1";
+					} else if (viewObject.classificationButton.isSelected()) {
+						state = "classification_step1";
+					} else if (viewObject.anomalyDetectionButton.isSelected()) {
+						state = "anomalyDetection_step1";
+					}
+					break;
+				case "clustering_step1":
+					state = "clustering_step2";
+					break;
+				case "clustering_step2":
+					state = "clustering_step3";
+					break;
+				case "clustering_step3":
+					state = "clustering_step4";
+					break;
+			}
+			viewObject.updateView(state);
 		}
-		
 	}
 
 }
--- src/View.java
+++ src/View.java
@@ -1,17 +1,23 @@
 import java.awt.*;
 
 import javax.swing.*;
-import javax.swing.border.*;
+//import javax.swing.border.*;
 
 public class View extends JFrame {
 	
 	// instance variables
 	private Controller controllerObject;
-	public JButton startOverButton, backButton, nextButton;
+	private JLabel infoLabel;
+	private JPanel topPanel, middlePanel, bottomPanel;
+	private boolean topInitiated, middleInitiated, bottomInitiated;
+	JButton startOverButton, backButton, nextButton;
+	JRadioButton mcfcAnalyticsFullDatasetButton, otherDatasetButton;
+	JRadioButton clusteringButton, classificationButton, anomalyDetectionButton;
 	
 	// constructor
 	public View(Controller controller) {
 		controllerObject = controller;
+		topInitiated = middleInitiated = bottomInitiated = false;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1140, 585);
@@ -19,68 +25,141 @@ public class View extends JFrame {
 		setLocation(40, 40);
 		setTitle("Machine Learning the Premier League");
 		
-		layoutTop();
-		layoutMiddle();
-		layoutBottom();
+		layoutMenu();
+		
+		updateView(controllerObject.getState());
 	}
 	
-	private void layoutTop() {
-		JPanel topPanel = new JPanel();
-		topPanel.setBorder(new TitledBorder(new EtchedBorder(), "Menu"));
-		//topPanel.setPreferredSize(new Dimension(1140, 150));
-		//top.setBackground(Color.lightGray);
-		
-		this.add(topPanel, BorderLayout.NORTH);
+	public void updateView(String state) {
+		layoutTop(state);
+		layoutMiddle(state);
+		layoutBottom(state);
 	}
 	
-	private void layoutMiddle() {
-		JLabel infoLabel = new JLabel();
-		infoLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		infoLabel.setFont(new Font("Serif", Font.BOLD + Font.ITALIC, 48));
-		infoLabel.setText("Which dataset do you wish to use?");
-		this.add(infoLabel, BorderLayout.CENTER);
+	private void layoutMenu() {
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu helpMenu = new JMenu("Help");
+		menuBar.add(helpMenu);
 	}
 	
-	private void layoutBottom() {
-		JPanel bottomPanel = new JPanel(new BorderLayout());
-		bottomPanel.setBorder(new TitledBorder(new EtchedBorder(), "User Selection Area"));
-		bottomPanel.setPreferredSize(new Dimension(1140, 100));
-		//bottom.setBackground(Color.lightGray);
-		
-		JPanel bottomCenterPanel = new JPanel();
-		
-		JRadioButton mcfcAnalyticsFullButton = new JRadioButton("MCFC Analytics Full Dataset");
-		mcfcAnalyticsFullButton.setSelected(true);
-		JRadioButton otherButton = new JRadioButton("Other");
-		
-		ButtonGroup group = new ButtonGroup();
-		group.add(mcfcAnalyticsFullButton);
-		group.add(otherButton);
-		
-		bottomCenterPanel.add(mcfcAnalyticsFullButton);
-		bottomCenterPanel.add(otherButton);
-		
-		bottomPanel.add(bottomCenterPanel, BorderLayout.CENTER);
-		
-		JPanel bottomSouthPanel = new JPanel();
-		bottomSouthPanel.setLayout(new BoxLayout(bottomSouthPanel, BoxLayout.LINE_AXIS));
-		bottomSouthPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
-		
-		startOverButton = new JButton("Start Over"); startOverButton.setEnabled(false);
-		startOverButton.addActionListener(controllerObject);
-		backButton = new JButton("Back"); backButton.setEnabled(false);
-		backButton.addActionListener(controllerObject);
-		nextButton = new JButton("Next");
-		nextButton.addActionListener(controllerObject);
-		
-		bottomSouthPanel.add(startOverButton);
-		bottomSouthPanel.add(Box.createHorizontalGlue());
-		bottomSouthPanel.add(backButton);
-		bottomSouthPanel.add(Box.createRigidArea(new Dimension(10, 0)));
-		bottomSouthPanel.add(nextButton);
-		
-		bottomPanel.add(bottomSouthPanel, BorderLayout.SOUTH);
-		
-		this.add(bottomPanel, BorderLayout.SOUTH);
+	private void layoutTop(String state) {
+		switch (state) {
+			case "startScreen_1":
+				if (!topInitiated) {
+					topPanel = new JPanel();
+					topPanel.setLayout(new GridBagLayout());
+					topPanel.setPreferredSize(new Dimension(1140, 270));
+					topPanel.setBackground(Color.CYAN);
+					//topPanel.setBorder(new TitledBorder(new EtchedBorder(), "Top"));
+					
+					infoLabel = new JLabel();
+					infoLabel.setFont(new Font("Serif", Font.BOLD + Font.ITALIC, 48));
+
+					topPanel.add(infoLabel);
+					
+					this.add(topPanel, BorderLayout.NORTH);
+					topInitiated = true;
+				}
+				infoLabel.setText("Which dataset do you wish to use?");
+				break;
+			case "startScreen_2":
+				infoLabel.setText("What task do you wish to perform?");
+				break;
+			case "clustering_step1":
+				infoLabel.setText("What data items do you wish to cluster?");
+				break;
+			case "clustering_step2":
+				infoLabel.setText("What features do you wish to cluster by?");
+				break;
+			case "clustering_step3":
+				infoLabel.setText("Please specify parameters for the K-Means algorithm.");
+				break;
+			case "clustering_step4":
+				infoLabel.setText("Results of the K-Means clustering algorithm");
+				break;
+		}
 	}
+	
+	private void layoutMiddle(String state) {
+		GridBagConstraints c = new GridBagConstraints();
+		if (middleInitiated) {
+			middlePanel.removeAll();
+			middlePanel.repaint();
+		}
+		switch (state) {
+			case "startScreen_1":
+				if (!middleInitiated) {
+					middlePanel = new JPanel();
+					middlePanel.setLayout(new GridBagLayout());
+					//middlePanel.setBorder(new TitledBorder(new EtchedBorder(), "Middle"));
+					this.add(middlePanel, BorderLayout.CENTER);
+					middleInitiated = true;
+				}
+				
+				mcfcAnalyticsFullDatasetButton = new JRadioButton("MCFC Analytics Full Dataset");
+				mcfcAnalyticsFullDatasetButton.setSelected(true);
+				otherDatasetButton = new JRadioButton("Other");
+				
+				ButtonGroup group = new ButtonGroup();
+				group.add(mcfcAnalyticsFullDatasetButton);
+				group.add(otherDatasetButton);
+				
+				middlePanel.add(mcfcAnalyticsFullDatasetButton, c);
+				middlePanel.add(otherDatasetButton, c);
+				break;
+			case "startScreen_2":
+				clusteringButton = new JRadioButton("Clustering");
+				clusteringButton.setSelected(true);
+				classificationButton = new JRadioButton("Classification");
+				anomalyDetectionButton = new JRadioButton("Anomaly Detection");
+				
+				ButtonGroup group2 = new ButtonGroup();
+				group2.add(clusteringButton);
+				group2.add(classificationButton);
+				group2.add(anomalyDetectionButton);
+				
+				middlePanel.add(clusteringButton, c);
+				middlePanel.add(classificationButton, c);
+				middlePanel.add(anomalyDetectionButton, c);
+				break;
+		}
+	}
+	
+	private void layoutBottom(String state) {
+		switch (state) {
+			case "startScreen_1":
+				if (!bottomInitiated) {
+					bottomPanel = new JPanel();
+					bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS));
+					bottomPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+					//bottomPanel.setBorder(new TitledBorder(new EtchedBorder(), "Bottom"));
+					
+					startOverButton = new JButton("Start Over");
+					startOverButton.addActionListener(controllerObject);
+					backButton = new JButton("Back");
+					backButton.addActionListener(controllerObject);
+					nextButton = new JButton("Next");
+					nextButton.addActionListener(controllerObject);
+					
+					bottomPanel.add(startOverButton);
+					bottomPanel.add(Box.createHorizontalGlue());
+					bottomPanel.add(backButton);
+					bottomPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+					bottomPanel.add(nextButton);
+					
+					this.add(bottomPanel, BorderLayout.SOUTH);
+					bottomInitiated = true;
+				}
+				startOverButton.setEnabled(false);
+				backButton.setEnabled(false);
+				break;
+			case "startScreen_2":
+				startOverButton.setEnabled(true);
+				backButton.setEnabled(true);
+				break;
+		}
+	}
+	
 }
