a5e3b9b4ef795b1b125c588f9c417cec3aa39514
victorgse <victorgse@gmail.com>
2014-08-03 16:21:29 +0100

    Updates to SVM Classification

--- src/algorithms/ClassificationAlgorithm.java
+++ src/algorithms/ClassificationAlgorithm.java
@@ -5,6 +5,9 @@ import java.util.Random;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Discretize;
+import weka.filters.unsupervised.attribute.NumericToNominal;
 
 /**
  * An abstract superclass which contains members that are common to all classification algorithms
@@ -17,7 +20,6 @@ public abstract class ClassificationAlgorithm extends Algorithm {
 	Classifier classifier;
 	Evaluation eval;
 	String evaluationOption;
-	Instances validationSet;
 	Instances testSet;
 	
 	/**
@@ -26,6 +28,36 @@ public abstract class ClassificationAlgorithm extends Algorithm {
 	 */
 	public void setTargetLabel(int classIndex) {
 		trainingSet.setClassIndex(classIndex);
+		if (!trainingSet.classAttribute().isNominal()) {
+			Instances newTrainingSet = null;
+			try {
+				NumericToNominal numericToNominalConverter = new NumericToNominal();
+				numericToNominalConverter.setAttributeIndices("last");
+				numericToNominalConverter.setInvertSelection(false);
+				numericToNominalConverter.setInputFormat(trainingSet);
+				newTrainingSet = Filter.useFilter(trainingSet, numericToNominalConverter);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (newTrainingSet.classAttribute().numValues() <= 5) {
+				trainingSet = newTrainingSet;
+			} else {
+				try {
+					Discretize numericToDiscreteNominalConverter = new Discretize();
+					numericToDiscreteNominalConverter.setAttributeIndices("last");
+					numericToDiscreteNominalConverter.setBins(5);
+					numericToDiscreteNominalConverter.setIgnoreClass(true);
+					numericToDiscreteNominalConverter.setInvertSelection(false);
+					numericToDiscreteNominalConverter.setUseEqualFrequency(true);
+					numericToDiscreteNominalConverter.setInputFormat(trainingSet);
+					trainingSet = Filter.useFilter(trainingSet, numericToDiscreteNominalConverter);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	/**
@@ -34,13 +66,20 @@ public abstract class ClassificationAlgorithm extends Algorithm {
 	public void setEvaluationOption(String evaluationOption) {
 		this.evaluationOption = evaluationOption;
 	}
-
+	
 	/**
-	 * Method for splitting a dataset into training, validation, and test sets (in specified proportions)
+	 * @return the testSet
+	 */
+	public Instances getTestSet() {
+		return testSet;
+	}
+	
+	/**
+	 * Method for splitting a dataset into training, validation, and test sets (in specified proportions).
 	 * @param trainingPercent
 	 * @param ValidationPercent
 	 * @param TestPercent
-	 */
+	 */ /*
 	public void splitDataset(double trainingPercent, double validationPercent, double testPercent) {
 		int trainingSetSize = (int) Math.round(trainingSet.numInstances() * trainingPercent);
 		int validationSetSize = (int) Math.round(trainingSet.numInstances() * validationPercent);
@@ -48,6 +87,18 @@ public abstract class ClassificationAlgorithm extends Algorithm {
 		Instances trainSet = new Instances(trainingSet, 0, trainingSetSize);
 		validationSet = new Instances(trainingSet, trainingSetSize, validationSetSize);
 		testSet = new Instances(trainingSet, trainingSetSize + validationSetSize, testSetSize);
+		trainingSet = trainSet;
+	} */
+
+	/**
+	 * Method for splitting a dataset into training and test sets (in specified proportions).
+	 * @param trainingPercent
+	 */
+	public void splitDataset(double trainingPercent) {
+		int trainingSetSize = (int) Math.round(trainingSet.numInstances() * trainingPercent);
+		int testSetSize = trainingSet.numInstances() - trainingSetSize;
+		Instances trainSet = new Instances(trainingSet, 0, trainingSetSize);
+		testSet = new Instances(trainingSet, trainingSetSize, testSetSize);
 		trainingSet = trainSet;
 	}
 
@@ -72,15 +123,10 @@ public abstract class ClassificationAlgorithm extends Algorithm {
 	public Evaluation evaluate() {
 		try {
 			eval = new Evaluation(trainingSet);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			if (evaluationOption.equals("CV")) {
+			if (evaluationOption.equals("trainingSet")) {
+				eval.evaluateModel(classifier, trainingSet);
+			} else if (evaluationOption.equals("CV")) {
 				eval.crossValidateModel(classifier, trainingSet, 10, new Random(1));
-			} else if (evaluationOption.equals("validationSet")) {
-				eval.evaluateModel(classifier, validationSet);
 			} else if (evaluationOption.equals("testSet")) {
 				eval.evaluateModel(classifier, testSet);
 			}
