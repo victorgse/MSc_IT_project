04c1c8affb32947cacec4bad5735b5a0eb504350
victorgse <victorgse@gmail.com>
2014-08-15 15:59:37 +0100

    Added abstract class OutlierDetectionAlgorithm.

---
+++ src/algorithms/outliers/OutlierDetectionAlgorithm.java
@@ -0,0 +1,15 @@
+package algorithms.outliers;
+
+import algorithms.Algorithm;
+
+public abstract class OutlierDetectionAlgorithm extends Algorithm {
+	
+	/*
+	@Override
+	public Object evaluate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	*/
+
+}
