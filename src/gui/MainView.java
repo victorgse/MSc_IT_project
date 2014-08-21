6ec8ceab8406890f092241fb5a91af1641bb97e3
victorgse <victorgse@gmail.com>
2014-08-21 22:06:18 +0100

    Code refactoring.

--- src/gui/MainView.java
+++ src/gui/MainView.java
@@ -3,7 +3,6 @@ package gui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
